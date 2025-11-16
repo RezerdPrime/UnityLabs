001de7b3bb29971e67c31e09ef433354f35c66a2
rezer <rezer@DESKTOP-0N31VAB>
2025-11-16 13:02:55 +1000

    Lab1

--- Assets/Scenes/CameraMovement.cs
+++ Assets/Scenes/CameraMovement.cs
@@ -5,27 +5,26 @@ public class CameraMovement : MonoBehaviour
 {
     [SerializeField] private Transform Player;
     [SerializeField] private Vector3 offset;
-    [SerializeField] private float cameraFollowSpeed = 2f;
+    [SerializeField] private float cameraFollowSpeed = 1.5f;
 
-    void Start()
-    {
-        if (Player != null)
-        {
-            offset = new Vector3(0, 9, -6);
-            transform.position = Player.position + offset;
-        }
-    }
+    //void Start()
+    //{
+    //    if (Player != null)
+    //    {
+    //        offset = new Vector3(0, 9, -6);
+    //        transform.position = Player.position + offset;
+    //    }
+    //}
 
-    // test
 
-    void Update()
-    {
-        if (Player == null) return;
+    //void Update()
+    //{
+    //    if (Player == null) return;
 
-        Vector3 targetPosition = Player.position + offset;
+    //    Vector3 targetPosition = Player.position + offset;
 
-        transform.position = Vector3.Lerp(transform.position, targetPosition, cameraFollowSpeed * Time.deltaTime);
+    //    transform.position = Vector3.Lerp(transform.position, targetPosition, cameraFollowSpeed * Time.deltaTime);
 
-    }
+    //}
 
 }
