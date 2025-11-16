b2cb59330da597cbc2c916f1e7ab05f92e20110b
rezer <rezer@DESKTOP-0N31VAB>
2025-11-16 11:34:37 +1000

    Добавьте файлы проекта.

---
+++ Assets/Scenes/CameraMovement.cs
@@ -0,0 +1,31 @@
+
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField] private Transform Player;
+    [SerializeField] private Vector3 offset;
+    [SerializeField] private float cameraFollowSpeed = 2f;
+
+    void Start()
+    {
+        if (Player != null)
+        {
+            offset = new Vector3(0, 9, -6);
+            transform.position = Player.position + offset;
+        }
+    }
+
+
+    void Update()
+    {
+        if (Player == null) return;
+
+        Vector3 targetPosition = Player.position + offset;
+
+        transform.position = Vector3.Lerp(transform.position, targetPosition, cameraFollowSpeed * Time.deltaTime);
+
+        //transform.LookAt(Player);
+    }
+
+}
